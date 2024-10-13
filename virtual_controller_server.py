08ea1d92363eae03eaa21f87a091cc9c050a39c6
turecross321 <51852312+turecross321@users.noreply.github.com>
2024-10-13 22:03:49 +0200

    Implement direction

--- virtual_controller_server.py
+++ virtual_controller_server.py
@@ -6,11 +6,16 @@ import websockets
 LISTEN_IP = "192.168.1.134"
 PORT = 1337
 
+DIRECTION_UP = 0
+DIRECTION_RIGHT = 1
+DIRECTION_DOWN = 2
+DIRECTION_LEFT = 3
+
 joystick = pyvjoy.VJoyDevice(1)
 joystick.set_axis(pyvjoy.HID_USAGE_X, 16384)  # Center position for X axis
 joystick.set_axis(pyvjoy.HID_USAGE_Y, 16384)  # Center position for Y axis
 
-move_direction = 1
+azimuth = 0.0
 move_speed = 0.0
 jump = False
 
@@ -19,12 +24,46 @@ def to_axis_value(float_value):
     return int(float_value * 16384 + 16384)
 
 
+def get_cardinal_direction(angle):
+    # Normalize the angle to be within 0-360
+    angle = angle % 360
+
+    # Determine the direction based on the angle
+    if angle >= 315 or angle < 45:
+        print("UP")
+        return DIRECTION_UP
+    elif 45 <= angle < 135:
+        print("RIGHT")
+        return DIRECTION_RIGHT
+    elif 135 <= angle < 225:
+        print("DOWN")
+        return DIRECTION_DOWN
+    elif 225 <= angle < 315:
+        print("LEFT")
+        return DIRECTION_LEFT
+
 def update_controller():
-    global joystick, move_direction, move_speed, jump
-    joystick.set_axis(pyvjoy.HID_USAGE_X, to_axis_value(move_speed * move_direction))  # move
+    global joystick, azimuth, move_speed, jump
+
+    direction = get_cardinal_direction(azimuth)
+    joystick_x_value = 0
+    joystick_y_value = 0
+
+    if direction == DIRECTION_UP:
+        joystick_y_value = move_speed
+    elif direction == DIRECTION_DOWN:
+        joystick_y_value = -move_speed
+    elif direction == DIRECTION_RIGHT:
+        joystick_x_value = move_speed
+    elif direction == DIRECTION_LEFT:
+        joystick_x_value = -move_speed
+
+    joystick.set_axis(pyvjoy.HID_USAGE_X, to_axis_value(joystick_x_value))
+    joystick.set_axis(pyvjoy.HID_USAGE_Y, to_axis_value(joystick_y_value))
+
+
     joystick.set_button(3, jump)  # jump
     # todo: grab
-    # todo: layer change
     # todo: menu navigation stuff
 
 
@@ -35,15 +74,19 @@ async def controller_loop():
 
 
 async def handle_client(websocket, path):
+    print("New connection at", path)
     async for message in websocket:
+        print(message)
         parsed = json.loads(message)
-        print(parsed)
-        print(path)
 
         if path == "/crocs":
             global move_speed, jump
             move_speed = parsed["speed"]
             jump = parsed["jump"]
+
+        elif path == "/compass":
+            global azimuth
+            azimuth = parsed["azimuth"]
 
 
 async def main():
